8f59ac6844adab76ca4168e7b628cc7a70bc8d6b
Dinesh S <dinesh.s@valtech.com>
2024-04-07 20:44:13 +0530

    homepage modified

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,52 +1,43 @@
 import React, { useState, useEffect } from "react";
-import "./home.css"; // Import CSS file for styling
 import axios from "axios";
+import "./home.css"; // Import CSS file for styling
+
 const Home = () => {
+  const [userProducts, setUserProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchUserProducts = async () => {
+      try {
+        const currentUser = localStorage.getItem('customer_id');
+        console.log(currentUser)
+        const response = await axios.get('http://localhost:5001/api/listuserproducts', { currentUser });
+        setUserProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching user products:', error);
+      }
+    };
+
+    fetchUserProducts();
+  }, []);
+
   return (
     <div>
       <div className="sliders">
         <div className="photo1"></div>
       </div>
-      <h1 id="products">Available Products</h1>
+      <h1 id="products">Assigned Products</h1>
       <div className="cardmain">
-        <div className="card">
-          <img
-            src="/Product_Images/DC_Earth_Resistance_Tester.jpg"
-            alt=""
-          />
-          <div className="details">
-            <a href="/phases">Status</a>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            src="/Product_Images/DC_Earth_Fault_Locator.jpg"
-            alt=""
-          />
-          <div className="details">
-            <a href="/phases">Status</a>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            src="/Product_Images/Insulation_Tester.jpg"
-            alt=""
-          />
-          <div className="details">
-            <a href="/phases">Status</a>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            src="/Product_Images/Offline_Fault_Locator.jpg"
-            alt=""
-          />
-          <div className="details">
-            <a href="/phases">Status</a>
-          </div>
-        </div>
-      </div>
+        {userProducts.map((product, index) => (
+          <div className="card" key={index}>
+            {/* <img src={`/Product_Images/${product.products_name}.jpg`} alt={product.products_name} /> */}
+            <img src={product.product_image_path} alt={product.products_name} />
 
+            <div className="details">
+              <a href="/phases">buy now</a>
+            </div>
+          </div>
+        ))}
+      </div>
       <div className="footer"></div>
     </div>
   );
